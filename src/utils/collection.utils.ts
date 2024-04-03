b7087c875362a4e426c012b0dbaa43a691711b50
hokageCV <chaitanyavaru@gmail.com>
2024-04-04 00:59:40 +0530

    Add project list

--- src/utils/collection.utils.ts
+++ src/utils/collection.utils.ts
@@ -4,3 +4,8 @@ export async function getBlogList() {
   const blogList = await getCollection('blogs')
   return blogList
 }
+
+export async function getProjectList() {
+  const projectList = await getCollection('projects')
+  return projectList
+}
