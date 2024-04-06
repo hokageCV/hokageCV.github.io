a369d5ee667649e757d8f0c544fcedf6b122da83
hokageCV <chaitanyavaru@gmail.com>
2024-04-06 16:59:55 +0530

    Add navbar

---
+++ src/data/navigation.ts
@@ -0,0 +1,27 @@
+const LINKS = {
+  blog: {
+    label: 'Blog',
+    url: '/blogs',
+  },
+  projects: {
+    label: 'Projects',
+    url: '/projects',
+  },
+  about: {
+    label: 'About',
+    url: '/about',
+  },
+  contact: {
+    label: 'Contact',
+    url: '/contact',
+  },
+}
+
+export default {
+  header: [
+    LINKS.blog,
+    LINKS.projects,
+    LINKS.about,
+    LINKS.contact,
+  ]
+}
