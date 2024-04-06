876c7d26ab2be48069e7655677088958687d3ddf
hokageCV <chaitanyavaru@gmail.com>
2024-04-06 18:20:38 +0530

    Add reset css & meta tags

---
+++ src/data/metadata.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Chaitanya Varu',
+  description: "Chaitanya Varu's Blog site",
+  url: 'https://hokageCV.github.io',
+  'og-image': 'https://res.cloudinary.com/dmtacem5p/image/upload/v1694933643/github/portfolio_open_graph_img.png'
+}
