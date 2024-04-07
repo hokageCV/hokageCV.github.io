d2829cb6a83cf2c7794952fb553de4745ce0010d
hokageCV <chaitanyavaru@gmail.com>
2024-04-07 14:58:20 +0530

    Add tags

---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type TagsType = Record<string, number>
--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -1,4 +1,6 @@
+import type { TagsType } from '@/types'
 import { getCollection } from 'astro:content'
+import _slugify from 'slugify'
 
 export async function getBlogList() {
   const blogList = await getCollection('blogs')
@@ -8,4 +10,21 @@ export async function getBlogList() {
 export async function getProjectList() {
   const projectList = await getCollection('projects')
   return projectList
+}
+
+export function getTags(blogPostList: any[]): Record<string, number> {
+  const tags: TagsType = {}
+
+  blogPostList.forEach((post) => {
+    post.data.tags && post.data.tags.forEach((tag: string) => {
+      if (!tags[tag]) { tags[tag] = 0 }
+      tags[tag]++
+    })
+  })
+
+  return tags
+}
+
+export function slugify(text: string) {
+  return _slugify(text, { lower: true })
 }
