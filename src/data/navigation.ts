978535f58f00c54e95f84a1c4d33ea67f24a1341
hokageCV <chaitanyavaru@gmail.com>
2024-04-10 12:42:28 +0530

    Add home page content

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,27 +1,31 @@
 const LINKS = {
+  // about: {
+  //   label: 'About',
+  //   url: '/about',
+  // },
   blog: {
     label: 'Blogs',
     url: '/blogs',
+  },
+  contact: {
+    label: 'Contact',
+    url: '/contact',
+  },
+  home: {
+    label: 'Home',
+    url: '/',
   },
   projects: {
     label: 'Projects',
     url: '/projects',
   },
-  // about: {
-  //   label: 'About',
-  //   url: '/about',
-  // },
-  contact: {
-    label: 'Contact',
-    url: '/contact',
-  },
 }
 
 export default {
   header: [
+    LINKS.home,
     LINKS.blog,
     LINKS.projects,
-    // LINKS.about,
     LINKS.contact,
   ]
 }
