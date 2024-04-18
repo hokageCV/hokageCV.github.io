91a129c6d10232bf51287d1b49122bf1da7fb51c
hokageCV <chaitanyavaru@gmail.com>
2024-04-18 23:18:29 +0530

    Add 'other posts you may like'

--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -30,3 +30,57 @@ export function getTags(blogPostList: any[]): Record<string, number> {
 export function slugify(text: string) {
   return _slugify(text, { lower: true })
 }
+
+export async function similarPosts(tag: string, excludingTitle: string = '') {
+  const blogPostList = await getBlogList()
+  let similarPosts = blogPostList.filter((post) => post.data.tags?.includes(tag))
+
+  if (excludingTitle !== '') return similarPosts = similarPosts.filter(post => post.data.title !== excludingTitle)
+
+  return similarPosts
+}
+
+export async function processedSimilarPosts(tags: string[], excludingTitle: string = '') {
+  const MINIMUM_NUMBER_OF_SIMILAR_POSTS = 3
+  const MAX_NUMBER_OF_SIMILAR_POSTS = 6
+  let otherPosts: any[] = []
+
+  // get posts of all tags
+  for (const tag of tags) {
+    const blogPostList = await similarPosts(tag, excludingTitle)
+    otherPosts = [...otherPosts, ...blogPostList]
+  }
+  otherPosts = removeDuplicatePosts(otherPosts)
+
+  if (otherPosts.length > MAX_NUMBER_OF_SIMILAR_POSTS) otherPosts.splice(MAX_NUMBER_OF_SIMILAR_POSTS);
+  if (otherPosts.length >= MINIMUM_NUMBER_OF_SIMILAR_POSTS) return otherPosts
+
+  const remainingPostsToFill = Math.abs(otherPosts.length - MINIMUM_NUMBER_OF_SIMILAR_POSTS)
+
+  let additionalPosts = await getBlogList()
+  additionalPosts = additionalPosts.filter(post => {
+    if (post.data.title === excludingTitle) return false
+
+    const postExistsInOtherPosts = otherPosts.some(otherPost => otherPost.data.slug === post.data.slug);
+    return !postExistsInOtherPosts;
+
+  })
+  additionalPosts.splice(remainingPostsToFill)
+
+  const combinedPosts = [...otherPosts, ...additionalPosts]
+  return combinedPosts
+}
+
+function removeDuplicatePosts(posts: any[]) {
+  const titleMap = new Map<string, any>()
+
+  for (const post of posts) {
+    const postTitle = post.data.title
+    if (!titleMap.has(postTitle)) {
+      titleMap.set(postTitle, post)
+    }
+  }
+
+  const uniquePosts = Array.from(titleMap.values())
+  return uniquePosts
+}
