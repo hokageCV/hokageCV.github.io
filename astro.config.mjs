5274724a02557751e6772c8eea3e0b0de463fd8b
hokageCV <chaitanyavaru@gmail.com>
2024-04-24 00:08:43 +0530

    Add GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hokageCV.github.io',
+  integrations: [partytown()],
 });
