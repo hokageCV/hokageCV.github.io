eab8a7023c2452bc042fa854c332ddf655a81f75
hokageCV <chaitanyavaru@gmail.com>
2025-01-26 11:13:28 +0530

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-import partytown from '@astrojs/partytown'
-import { defineConfig } from 'astro/config'
-import og from './src/utils/open-graph'
+import partytown from '@astrojs/partytown';
+import { defineConfig } from 'astro/config';
+import og from './src/utils/open-graph';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hokageCV.github.io',
-  integrations: [partytown(), og()],
+  integrations: [partytown(), og(), sitemap()],
   trailingSlash: 'never',
   markdown: {
     shikiConfig: {
       themes: {
         light: 'solarized-dark',
-        dark: 'solarized-dark',
+        dark: 'solarized-dark'
       },
-      wrap: true,
-    },
-  },
-})
+      wrap: true
+    }
+  }
+});
